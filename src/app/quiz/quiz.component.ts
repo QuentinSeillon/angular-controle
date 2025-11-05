b252f3863c8952ea1f1e90ea6dedd80c0741b850
QuentinSeillon <quentin.seillon@gmail.com>
2025-11-05 11:21:30 +0100

    modif qui component

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -24,8 +24,6 @@ export class QuizComponent implements OnInit {
     this.playerName = this.route.snapshot.paramMap.get('playerName') ?? '';
     this.route.queryParamMap.subscribe(params => {
       this.category = params.get('category') ?? undefined;
-      // Appelle ton service pour charger les questions de this.category
-      // ex: this.quizService.loadByCategory(this.category)
     });
   }
 
